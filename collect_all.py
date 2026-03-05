adf772353e79a40bdf5dad4248cb612e4a34e337
Tomer Stoianov <tomer.stoianov2@gmail.com>
2026-03-05 18:06:37 +0200

    Update

--- collect_all.py
+++ collect_all.py
@@ -5,13 +5,18 @@ output_filename = "files_list.txt"
 script_name = os.path.basename(sys.argv[0])
 root_dir = "."
 
+# רשימת תיקיות שצריך לדלג עליהן כדי לא להעמיס על הקובץ
+ignore_dirs = {'.git', 'node_modules', 'dist', 'build', '.vite'}
+# רשימת קבצים ספציפיים שאין צורך לקרוא (הם סתם ארוכים)
+ignore_files = {output_filename, script_name, ".DS_Store", "package-lock.json"}
+
 with open(output_filename, "w", encoding="utf-8") as f:
     for root, dirs, files in os.walk(root_dir):
-        if '.git' in dirs:
-            dirs.remove('.git')
-            
+        # מחיקת התיקיות מהרשימה כדי ש-os.walk לא ייכנס אליהן בכלל
+        dirs[:] = [d for d in dirs if d not in ignore_dirs]
+        
         for file in files:
-            if file in (output_filename, script_name, ".DS_Store"):
+            if file in ignore_files:
                 continue
             
             full_path = os.path.join(root, file)
